43221b40fd25d7cbc790c0b31e982aacd1412e36
sampson-q <quarmysokpoli@gmail.com>
2023-10-24 01:36:55 +0000

    interface commit check

--- src/BankMateMain.java
+++ src/BankMateMain.java
@@ -1,24 +1,37 @@
-import controllers.UserSignup;
+import loan.Loans;
+
+import java.util.Scanner;
 public class BankMateMain {
-
+    static Scanner choiceInput = new Scanner(System.in);
     public static void main(String[] args) {
-        // Create an instance of the UserSignup class
-        //UserSignup userSignup = new UserSignup();
+        System.out.println("\n========================================\nW e l c o m e    t o    B a n k M a t e\n\t\tyour financial companion\n========================================");
+        System.out.println("   [1] Create Account\n   [2] Loans\n   [3] Withdrawal\n   [4] Deposit\n   [5] Transfers");
+        System.out.print("\nEnter Choice: ");
+        String choice = choiceInput.nextLine();
 
-        // Define user information
-        //String fullName = "John Doe";
-        //String phoneNumber = "1234567890";
-        //String pinPassword = "2800";
+        choiceHandler(choice);
+    }
 
-        // Call the createUser method to create a new user
-        //boolean userCreated = userSignup.createUser(fullName, phoneNumber, pinPassword);
-
-        //if (userCreated) {
-        //    System.out.println("User created successfully!");
-        //} else {
-        //    System.out.println("User creation failed.");
-        //}
-
-        System.out.println("Welcome to BankMate");
+    static void choiceHandler(String choice) {
+        switch (choice) {
+            case "1":
+                System.out.println("Create Account?");
+                break;
+            case "2":
+                new Loans().displayHeader();
+                new Loans().displayOptions();
+                break;
+            case "3":
+                System.out.println("Withdrawal?");
+                break;
+            case "4":
+                System.out.println("Deposit?");
+                break;
+            case "5":
+                System.out.println("Transfers?");
+                break;
+            default:
+                System.out.println("Wrong choice. Try again.");
+        }
     }
 }
