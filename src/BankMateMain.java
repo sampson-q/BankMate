ed293638ae6c4b21ed28160b1f79bfe8ec3f98a0
sampson-q <quarmysokpoli@gmail.com>
2023-10-24 00:29:30 +0000

    user signup...

---
+++ src/BankMateMain.java
@@ -0,0 +1,22 @@
+import controllers.UserSignup;
+public class BankMateMain {
+
+    public static void main(String[] args) {
+        // Create an instance of the UserSignup class
+        UserSignup userSignup = new UserSignup();
+
+        // Define user information
+        String fullName = "John Doe";
+        String phoneNumber = "1234567890";
+        String pinPassword = "2800";
+
+        // Call the createUser method to create a new user
+        boolean userCreated = userSignup.createUser(fullName, phoneNumber, pinPassword);
+
+        if (userCreated) {
+            System.out.println("User created successfully!");
+        } else {
+            System.out.println("User creation failed.");
+        }
+    }
+}
