879372d242b708a34c6f463e983e8924212cde0e
sampson-q <quarmysokpoli@gmail.com>
2023-10-24 00:42:02 +0000

    interface commit check

--- src/BankMateMain.java
+++ src/BankMateMain.java
@@ -3,20 +3,22 @@ public class BankMateMain {
 
     public static void main(String[] args) {
         // Create an instance of the UserSignup class
-        UserSignup userSignup = new UserSignup();
+        //UserSignup userSignup = new UserSignup();
 
         // Define user information
-        String fullName = "John Doe";
-        String phoneNumber = "1234567890";
-        String pinPassword = "2800";
+        //String fullName = "John Doe";
+        //String phoneNumber = "1234567890";
+        //String pinPassword = "2800";
 
         // Call the createUser method to create a new user
-        boolean userCreated = userSignup.createUser(fullName, phoneNumber, pinPassword);
+        //boolean userCreated = userSignup.createUser(fullName, phoneNumber, pinPassword);
 
-        if (userCreated) {
-            System.out.println("User created successfully!");
-        } else {
-            System.out.println("User creation failed.");
-        }
+        //if (userCreated) {
+        //    System.out.println("User created successfully!");
+        //} else {
+        //    System.out.println("User creation failed.");
+        //}
+
+        System.out.println("Welcome to BankMate");
     }
 }
