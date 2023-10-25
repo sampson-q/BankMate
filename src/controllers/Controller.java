d97433f06ff5d38930a8f898489047a5e6679317
sampson-q <quarmysokpoli@gmail.com>
2023-10-25 13:38:37 +0000

    setters and getters for session management

--- src/controllers/Controller.java
+++ src/controllers/Controller.java
@@ -2,6 +2,7 @@ package controllers;
 
 import dbConnectivity.DatabaseConnection;
 
+import java.math.BigDecimal;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
@@ -11,6 +12,9 @@ import java.sql.SQLException;
 import java.util.logging.Logger;
 
 public class Controller {
+    String activeAccountNumber, activeAccountID, activeAccountName;
+    Integer activeAccountLoanID;
+    BigDecimal activeAccountBalance, activeAccountLoanBalance;
     private static final Logger logger = Logger.getLogger(UserSignup.class.getName());
 
     // Validate the user for signup
@@ -78,5 +82,140 @@ public class Controller {
             logger.log(java.util.logging.Level.SEVERE, "Error checking if user exists:", e);
         }
         return false; // Default to false in case of an error
+    }
+
+    public String getActiveAccountID() {
+        return activeAccountID;
+    }
+
+    public void setActiveAccountID() {
+        String accountNumberSQL = "SELECT account_id FROM accounts WHERE phone_number = ?";
+        try (Connection connection = DatabaseConnection.getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(accountNumberSQL)) {
+            preparedStatement.setString(1, getActiveAccountNumber());
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+
+            if (resultSet.next()) {
+                this.activeAccountID = resultSet.getString("account_id"); // set up account ID
+            } else {
+                System.out.println("Account not found for the given phone number.");
+            }
+
+            // Close resources
+            preparedStatement.close();
+            resultSet.close();
+        } catch (SQLException e) {
+            logger.log(java.util.logging.Level.SEVERE, "An error occurred during account number fetch:", e);
+        }
+    }
+
+    public String getActiveAccountName() {
+        return activeAccountName;
+    }
+
+    public void setActiveAccountName() {
+        String fullNameSQL = "SELECT full_name FROM users WHERE phone_number = ?";
+
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(fullNameSQL)) {
+            preparedStatement.setString(1, getActiveAccountNumber());
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                this.activeAccountName = resultSet.getString("full_name");
+            }
+
+            // Close resources
+            resultSet.close();
+        } catch (SQLException e) {
+            logger.log(java.util.logging.Level.SEVERE, "An error occurred during full name fetch:", e);
+        }
+    }
+
+    public BigDecimal getActiveAccountBalance() {
+        return activeAccountBalance;
+    }
+
+    public void setActiveAccountBalance() {
+        String accountBalanceSQL = "SELECT account_balance FROM accounts WHERE phone_number = ?";
+
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(accountBalanceSQL)) {
+            preparedStatement.setString(1, getActiveAccountNumber());
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                this.activeAccountBalance = resultSet.getBigDecimal("account_balance");
+            }
+
+            // Close resources
+            resultSet.close();
+        } catch (SQLException e) {
+            logger.log(java.util.logging.Level.SEVERE, "An error occurred during account balance fetch:", e);
+        }
+
+    }
+
+    public BigDecimal getActiveAccountLoanBalance() {
+        return activeAccountLoanBalance;
+    }
+
+    public void setActiveAccountLoanBalance() {
+        String loanBalanceSQL = "SELECT L.loan_balance " +
+                "FROM loans L " +
+                "JOIN accounts A ON L.account_id = A.account_id " +
+                "JOIN users U ON A.phone_number = U.phone_number " +
+                "WHERE U.phone_number = ?";
+
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(loanBalanceSQL)) {
+            preparedStatement.setString(1, getActiveAccountNumber());
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                this.activeAccountLoanBalance = resultSet.getBigDecimal("loan_balance");
+            }
+
+            // Close resources
+            resultSet.close();
+        } catch (SQLException e) {
+            logger.log(java.util.logging.Level.SEVERE, "An error occurred during loan balance fetch:", e);
+        }
+    }
+
+    public Integer getActiveAccountLoanID() {
+        return activeAccountLoanID;
+    }
+
+    public void setActiveAccountLoanID() {
+        String loanIdSQL = "SELECT L.loan_id " +
+                "FROM loans L " +
+                "JOIN accounts A ON L.account_id = A.account_id " +
+                "JOIN users U ON A.phone_number = U.phone_number " +
+                "WHERE U.phone_number = ?";
+
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(loanIdSQL)) {
+            preparedStatement.setString(1, getActiveAccountNumber());
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                this.activeAccountLoanID = resultSet.getInt("loan_id");
+            }
+
+            // Close resources
+            resultSet.close();
+        } catch (SQLException e) {
+            logger.log(java.util.logging.Level.SEVERE, "An error occurred during loan ID fetch:", e);
+        }
+
+    }
+
+    public void setActiveAccountNumber(String activeAccountNumber) {
+        this.activeAccountNumber = activeAccountNumber;
+    }
+
+    public String getActiveAccountNumber() {
+        return this.activeAccountNumber;
     }
 }
