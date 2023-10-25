2d00220721915893998943a66c49aea52698d980
sampson-q <quarmysokpoli@gmail.com>
2023-10-25 14:27:37 +0000

    collaboration-wise configuration

--- src/dbConnectivity/DatabaseConnection.java
+++ src/dbConnectivity/DatabaseConnection.java
@@ -5,6 +5,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
+    // uncomment likes 9 and 10
+    // String dbIP = "0.4";
+    // private static final String JDBC_URL = "jdbc:mysql://192.168.:3306/b_mate";
+
+    // comment line 13
     private static final String JDBC_URL = "jdbc:mysql://localhost:3306/b_mate";
     private static final String DB_USER = "root";
     private static final String DB_PASSWORD = "";
