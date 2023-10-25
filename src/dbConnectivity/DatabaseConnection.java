c05ffbaeac4882de72db7fc42184b508f08a036a
Chvsmi <whisdomamedzrovi@gmail.com>
2023-10-25 16:13:42 +0100

    setup

--- src/dbConnectivity/DatabaseConnection.java
+++ src/dbConnectivity/DatabaseConnection.java
@@ -5,17 +5,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    // uncomment likes 10 to 13
-
      static String dbIP = "0.4";
      private static final String JDBC_URL = "jdbc:mysql://192.168." + dbIP + ":3306/b_mate";
      private static final String DB_USER = "hash";
      private static final String DB_PASSWORD = "b_mateProduction";
-
-    // comment line 16, 17, 18
-//    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/b_mate";
-//    private static final String DB_USER = "root";
-//    private static final String DB_PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASSWORD);
